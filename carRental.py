4ce8c6942cf523ae760d5eb1d020fb01f27b49ca
christophernguyen6089 <christopher.nguyen10@mavs.uta.edu>
2022-12-02 15:16:13 -0600

    implemented more of the python script. still yet to implement the meat

--- carRental.py
+++ carRental.py
@@ -4,5 +4,26 @@ import sqlite3
 
 #create tkinter window
 root = Tk()
-root.title('Address Book')
+root.title('Car Rental DB')
 root.geometry("400x400")
+
+#connect to the DB
+#conn is an object that holds the connection for the DB
+conn = sqlite3.connect('carrental.db')
+print("Connected to DB Successfully")
+
+
+#create a cursor
+carrental_cursor = conn.cursor()
+
+def submit():
+    #TODO: process for inserting some stuff into the DB
+
+def input_query():
+    #TODO: process for using a select query
+
+#TODO: define all GUI components (using a grid)
+
+#TODO: labels
+
+#TODO: submit button and their locations
